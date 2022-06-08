1648f729cbf86ba9eb97840e3a99e6f901e8c47b
Aditya5903 <96542462+Aditya5903@users.noreply.github.com>
2022-06-08 17:55:50 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,10 @@
+#This program add two numbers
+# Store input numbers
+A = input('Enter first number: ')
+B = input('Enter second number: ')
+
+# Add two numbers
+sum = float(A) + float(B)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(A, B, sum))
