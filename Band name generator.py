0cc9346459ac3af0aa1b69bd34960fc172ee44ec
Aditya5903 <96542462+Aditya5903@users.noreply.github.com>
2022-06-15 16:01:37 +0530

    Delete Band name generator.py

--- Band name generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the band name generator: ")
-city = input("First three letter of your city?\n")
-pet = input("First three letter of your pet's name?\n")
-print("Your band name should be " + city + " " + pet)
